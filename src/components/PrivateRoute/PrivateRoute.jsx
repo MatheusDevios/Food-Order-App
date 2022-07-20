336cd27d00568ebc2c5615985c372e8fce06c90f
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-07-20 13:17:35 +0100

    fixed error

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = (props) => {
   const authIsLoggedIn = localStorage.getItem("token");
 
-  return authIsLoggedIn ? props.children : <Navigate to="/" />;
+  return authIsLoggedIn ? props.children : <Navigate to="/auth" />;
 };
 
 export default PrivateRoute;
